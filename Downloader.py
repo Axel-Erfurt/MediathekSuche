6a63a9469d1cca54995b774165cab1eaf1f94f8d
Axel Schneider <axel99092@t-online.de>
2020-01-12 14:09:11 +0100

    Add files via upload

---
+++ Downloader.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+import queue     #If this template is not loaded, pyinstaller may not be able to run the requests template after packaging
+import requests
+################################################
+
+
+################################################
+class Downloader(QWidget):
+    def __init__(self, *args, **kwargs):
+        super(Downloader, self).__init__(*args, **kwargs)
+        
+        self.setStyleSheet("QWidget {background: qlineargradient(y1: 0, y2: 1, \
+                                 stop: 0 #E1E1E1, stop: 0.4 #DDDDDD, \
+                                 stop: 0.5 #D8D8D8, stop: 1.0 #D3D3D3);} \
+                                 QPushButton {background: #babdb6;} \
+                                 QLabel {background: transparent;}")
+        layout = QVBoxLayout(self)
+        hlayout = QHBoxLayout()
+        
+        #self.homepath = QDir.homePath() + "/Downloads/"
+        self.root = QFileInfo(__file__).absolutePath()
+        self.homepath = ""
+        folder = self.root + "/DLOrdner.txt"
+        #print("folder", folder)
+        with open(folder, 'r') as f:
+            t = f.read()
+            self.homepath = t
+            
+            if t == "":
+                fd = QDir.homePath() + "/Downloads/"
+                with open(folder, 'w') as f:
+                    f.write(fd)
+                    t = fd
+            
+        print("Download Ordner:", t)
+        self.url = ""
+        self.fname = ""
+
+        # Download Button
+        self.pushButton = QPushButton(self, minimumWidth=100)
+        self.pushButton.setText("Download")
+        self.pushButton.setIcon(QIcon.fromTheme("download"))
+        hlayout.addWidget(self.pushButton)
+        self.pushButton.clicked.connect(self.on_pushButton_clicked)
+        
+        # Cancel Button
+        self.cancelButton = QPushButton(self, minimumWidth=100)
+        self.cancelButton.setText("Cancel")
+        self.cancelButton.setIcon(QIcon.fromTheme("cancel"))
+        hlayout.addWidget(self.cancelButton)
+        self.cancelButton.setVisible(False)
+        self.cancelButton.clicked.connect(self.on_cancelButton_clicked)
+        
+        # Bar
+        self.progressBar = QProgressBar(self, minimumWidth=400)
+        self.progressBar.setValue(0)
+        #self.progressBar.setFixedHeight(12)
+        self.progressBar.setStyleSheet("QProgressBar {font-size: 7pt;}")
+        hlayout.addWidget(self.progressBar)
+        
+        self.lbl = QLabel("status")
+        layout.addLayout(hlayout)
+        layout.addWidget(self.lbl)
+        
+        self.lbl.setStyleSheet("QLabel {font-size: 8pt; color: #888a85}")
+        self.lbl.setText("Ready")
+
+
+    def on_pushButton_clicked(self):
+        the_url = self.url
+        the_filesize = requests.get(the_url, stream=True).headers['Content-Length']
+        the_filepath = self.homepath + self.fname
+        the_fileobj = open(the_filepath, 'wb')
+        #### Create a download thread
+        self.downloadThread = downloadThread(the_url, the_filesize, the_fileobj, buffer=10240)
+        self.downloadThread.download_proess_signal.connect(self.set_progressbar_value)
+        self.downloadThread.start()
+        self.lbl.setText("Download started ...")
+        self.cancelButton.setVisible(True)
+
+
+
+    # Setting progress bar
+    def set_progressbar_value(self, value):
+        self.progressBar.setValue(value)
+        if value == 100:
+            QMessageBox.information(self, "Tips", "Download success!")
+            self.close()
+
+
+    def on_cancelButton_clicked(self):
+        self.downloadThread.terminate()
+        self.lbl.setText("Download cancelled")
+        self.cancelButton.setVisible(False)
+
+
+##################################################################
+#Download thread
+##################################################################
+class downloadThread(QThread):
+    download_proess_signal = pyqtSignal(int)                        #Create signal
+
+    def __init__(self, url, filesize, fileobj, buffer):
+        super(downloadThread, self).__init__()
+        self.url = url
+        self.filesize = filesize
+        self.fileobj = fileobj
+        self.buffer = buffer
+
+
+    def run(self):
+        try:
+            rsp = requests.get(self.url, stream=True)                #Streaming download mode
+            offset = 0
+            for chunk in rsp.iter_content(chunk_size=self.buffer):
+                if not chunk: break
+                self.fileobj.seek(offset)                            #Setting Pointer Position
+                self.fileobj.write(chunk)                            #write file
+                offset = offset + len(chunk)
+                proess = offset / int(self.filesize) * 100
+                self.download_proess_signal.emit(int(proess))        #Sending signal
+            #######################################################################
+            self.fileobj.close()    #Close file
+            self.exit(0)            #Close thread
+
+
+        except Exception as e:
+            print(e)
+
+####################################
+#Program entry
+####################################
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = Downloader()
+    w.setWindowTitle("Downloader")
+    w.show()
+    sys.exit(app.exec_())
---
+++ MediathekSuche.py
@@ -0,0 +1,356 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+### © Axel Schneider 2020 ###
+### Credits: https://github.com/mediathekview/mediathekviewweb (API used) ###
+### GNU General Public License v3.0 ###
+
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import (QFileInfo, Qt, QSettings, QSize, QFile, QModelIndex)
+from PyQt5.QtWidgets import (QMainWindow, QTableWidget, QGridLayout, QPushButton, 
+                             QAbstractItemView, QAction, QLineEdit, QWidget, QLabel, 
+                             QComboBox, QMessageBox, QApplication,  QTableWidgetItem, QCheckBox)
+import MediathekPlayer
+import Downloader
+import pandas as pd
+#import csv, codecs
+import requests
+###################################
+btnWidth = 110
+btnHeight = 22
+
+        
+class MyWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__()
+        self.setObjectName("MediathekQuery")
+        self.root = QFileInfo(__file__).absolutePath()
+        self.setAttribute(Qt.WA_DeleteOnClose)
+        self.settings = QSettings('Axel Schneider', self.objectName())
+        self.viewer = QTableWidget()
+        
+        self.titleList = []
+        self.topicList = []
+        self.urlList = []
+        self.urlKleinList = []
+        self.beschreibungList = []
+        self.idList = []
+        self.chList = []
+        self.results = ""
+        
+        self.myurl = ""
+        self.fname = ""
+        self.viewer.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.viewer.SelectionMode(QAbstractItemView.SingleSelection)
+
+        self.viewer.verticalHeader().setStretchLastSection(False)
+        self.viewer.horizontalHeader().setStretchLastSection(True)
+
+        self.viewer.setColumnCount(6)
+        self.viewer.setColumnWidth(0, 48)
+        self.viewer.setColumnWidth(1, 150)
+        self.viewer.setColumnWidth(2, 200)
+        self.viewer.hideColumn(3)
+        self.viewer.hideColumn(4)
+        self.viewer.setHorizontalHeaderLabels(["Sender", "Thema", "Titel", "HD", "SD", "Beschreibung"])
+
+        self.viewer.verticalHeader().setVisible(True)
+        self.viewer.horizontalHeader().setVisible(True)
+        self.setStyleSheet(stylesheet(self))        
+        self.viewer.selectionModel().selectionChanged.connect(self.getCellText)
+        
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.viewer,0, 0, 1, 6)
+
+        self.findfield = QLineEdit()
+        self.fAction = QAction(QIcon.fromTheme("edit-clear"), "", triggered = self.findfieldAction)
+        self.findfield.addAction(self.fAction, 1)
+        self.findfield.returnPressed.connect(self.myQuery)
+        self.findfield.setFixedWidth(200)
+        self.findfield.setPlaceholderText("suchen ...")
+        self.findfield.setToolTip("ENTER to find")
+        self.layout.addWidget(self.findfield,1, 0)
+        
+        self.chCombo = QComboBox()
+        self.chCombo.setFixedWidth(66)
+        self.chCombo.addItems(['ARD', 'ZDF', 'MDR', 'PHOENIX', 'RBB', 'BR', 'HR', 'SR', \
+                               'SWR', 'NDR', 'DW', 'WDR', 'ARTE', '3SAT', 'KIKA', 'ORF'])
+        self.chCombo.addItem("alle")
+        self.chCombo.setToolTip("Sender wählen")
+        self.chCombo.currentIndexChanged.connect(self.myQuery)
+        self.layout.addWidget(self.chCombo,1, 1)
+        
+        self.btnPlay = QPushButton("Play")
+        self.btnPlay.setFixedWidth(80)
+        self.btnPlay.setIcon(QIcon.fromTheme("media-playback-start"))
+        self.layout.addWidget(self.btnPlay,1, 2)
+        self.btnPlay.clicked.connect(self.playVideo)
+        
+        self.btnDownload = QPushButton("Download")
+        self.btnDownload.setFixedWidth(100)
+        self.btnDownload.setIcon(QIcon.fromTheme("download"))
+        self.layout.addWidget(self.btnDownload,1, 3)
+        self.btnDownload.clicked.connect(self.downloadVideo)
+        
+        self.chBox = QPushButton("SD")
+        self.chBox.setToolTip("umschalten HD / SD")
+        self.chBox.setStyleSheet("background: #729fcf;")
+        self.chBox.setFixedWidth(44)
+        self.chBox.clicked.connect(self.toggleQuality)
+        self.layout.addWidget(self.chBox,1, 4)
+        
+        self.lbl = QLabel("Info")
+        self.layout.addWidget(self.lbl,1, 5)
+
+        self.myWidget = QWidget()
+        self.myWidget.setLayout(self.layout)
+
+        self.msg("Ready")
+        self.setCentralWidget(self.myWidget)
+        self.setWindowIcon(QIcon.fromTheme("office-database"))
+        self.setGeometry(20,20,600,450)
+        self.setWindowTitle("Mediathek")
+        self.readSettings()
+        self.msg("Ready")
+        self.findfield.setFocus()
+        self.player = MediathekPlayer.VideoPlayer('')
+        self.player.hide()
+        
+    def toggleQuality(self):
+        if self.chBox.text() == "SD":
+            self.chBox.setText("HD")
+            self.chBox.setStyleSheet("background: #8ae234;")
+            self.getCellText()
+        else:
+            self.chBox.setText("SD")
+            self.chBox.setStyleSheet("background: #729fcf;")
+            self.getCellText()
+        
+    def myQuery(self):
+        if not self.findfield.text() == "":
+            self.viewer.setRowCount(0)
+            self.viewer.clearContents()
+            self.titleList = []
+            self.topicList = []
+            self.urlList = []
+            self.urlKleinList = []
+            self.beschreibungList = []
+            self.idList = []
+            self.chList = []
+
+            channels = [self.chCombo.currentText()]
+            if channels == ["alle"]:
+                channels = ["ard", "zdf", "mdr", "phoenix", "rbb", "br", "hr", "sr", "swr", "ndr",\
+                            "dw", "wdr", "arte", "3sat", "kika", "orf"]
+            print("suche",  self.findfield.text(), "in", ','.join(channels).upper())
+            for ch in channels:
+                r = self.makeQuery(ch, self.findfield.text())
+
+            for b in range(len(self.titleList)):
+                self.idList.append(str(b))
+                    
+            for x in range(len(self.titleList)):
+                #print(f"{self.idList[x]}\t{self.chList[x]}\t{self.topicList[x]}\t{self.titleList[x]}\t{self.urlList[x]}")
+                self.viewer.insertRow(x)
+                self.viewer.setItem(x, 0, QTableWidgetItem(self.chList[x]))
+                self.viewer.setItem(x, 1, QTableWidgetItem(self.topicList[x]))
+                self.viewer.setItem(x, 2, QTableWidgetItem(self.titleList[x]))
+                self.viewer.setItem(x, 3, QTableWidgetItem(self.urlList[x]))
+                self.viewer.setItem(x, 4, QTableWidgetItem(self.urlKleinList[x]))
+                self.viewer.setItem(x, 5, QTableWidgetItem(self.beschreibungList[x]))
+            for x in range(len(self.titleList)):
+                self.viewer.resizeRowToContents(x)
+        
+        
+    def makeQuery(self, channel, myquery):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0',
+            'Accept': '*/*',
+            'Accept-Language': 'de-DE,en;q=0.5',
+            'Content-Type': 'text/plain;charset=UTF-8',
+            'Connection': 'keep-alive',
+        }
+        
+        data = {"size":"500", "sortBy":"timestamp", "sortOrder":"desc", "queries":[{"fields":["title", "topic", "description"],
+                            "query":"" + myquery + ""},{"fields":["channel"],
+                            "query":"" + channel + ""}]}
+        
+        response = requests.post('https://mediathekviewweb.de/api/query', headers=headers, json=data)
+        response_json = response.json()
+        count = int(response_json['result']['queryInfo']['resultCount'])
+        for x in range(count):
+            topic = response_json['result']['results'][x]['topic']
+            title = response_json['result']['results'][x]['title']
+            url = response_json['result']['results'][x]['url_video']
+            url_klein = response_json['result']['results'][x]['url_video_low']
+            beschreibung = response_json['result']['results'][x]['description']
+            ch = response_json['result']['results'][x]['channel']
+            self.titleList.append(title)
+            self.topicList.append(topic)
+            self.urlList.append(url)
+            self.chList.append(ch)
+            self.urlKleinList.append(url_klein)
+            self.beschreibungList.append(beschreibung)
+        print(count, "Beiträge gefunden")
+        self.lbl.setText(f"{count} Beiträge gefunden")
+        
+    def findfieldAction(self):
+        self.findfield.setText("")
+        
+    def downloadVideo(self):
+        if not self.url == "":
+            self.downloader = Downloader.Downloader()
+            self.downloader.setWindowTitle("Downloader")
+            self.downloader.url = self.url
+            item = self.viewer.selectedIndexes()[2]
+            if not item == None:
+                filename = str(item.data())
+            self.downloader.fname = filename + self.url[-4:]
+            self.downloader.lbl.setText("speichern als: " + self.downloader.homepath + self.downloader.fname)
+            self.downloader.move(self.x() + 2, self.y() + 28)
+            self.downloader.show()
+        else:
+            print("keine URL")
+            self.msg("keine URL")
+        
+        
+    def getCellText(self):
+        if self.viewer.selectionModel().hasSelection():
+            row = self.selectedRow()
+            if not self.chBox.text() == "SD":
+                item = self.urlList[row]
+            else:
+                item = self.urlKleinList[row]
+            if not item == None:
+                name = item
+                self.url = str(item)
+                print(self.url)
+            infotext = f"{self.chList[row]}: {self.topicList[row]} - {self.titleList[row]} ({self.chBox.text()})"
+            self.msg(infotext)
+            self.fname = str(self.viewer.selectedIndexes()[1].data())
+
+        
+    def playVideo(self):
+        if self.viewer.selectionModel().hasSelection():
+            row = self.selectedRow()
+            if not self.chBox.text() == "SD":
+                item = item = self.urlList[row]
+                print("play HD")
+            else:
+                item = self.urlKleinList[row]
+                print("play SD")
+            if not item == None:
+                self.url = item
+                if not self.url == "":
+                    print("url =", self.url)
+                    self.player.show()
+                    self.player.playMyURL(self.url)
+                else:
+                    print("keine URL vorhanden")
+                    self.msg("keine URL vorhanden")
+            else:
+                print("keine URL vorhanden")
+        else:
+            print("keine URL vorhanden")
+
+    def selectedRow(self):
+        if self.viewer.selectionModel().hasSelection():
+            row =  self.viewer.selectionModel().selectedIndexes()[0].row()
+            return int(row)           
+
+    def closeEvent(self, e):
+        self.writeSettings()
+        self.player.close()
+        e.accept()
+
+    def readSettings(self):
+        print("lese Fensterposition")
+        if self.settings.contains('geometry'):
+            self.setGeometry(self.settings.value('geometry'))
+
+    def writeSettings(self):
+        print("Fensterposition gespeichert")
+        self.settings.setValue('geometry', self.geometry())
+
+    def msg(self, message):
+        self.statusBar().showMessage(message, 0)
+
+
+def stylesheet(self):
+        return """
+        QTableWidget
+        {
+            border: 1px solid grey;
+            border-radius: 0px;
+            font-size: 8pt;
+            background-color: #d2d2d2;
+            selection-background-color: #1a2334;
+            selection-color: #ffffff;
+        }
+
+
+        QHeaderView::section
+        {background-color:#d3d7cf;
+        color: #2e3436; 
+        font: bold
+        }
+        QTableCornerButton::section 
+        {
+        background-color:#d3d7cf; 
+        }
+
+        QStatusBar
+        {
+            font-size: 8pt;
+            color: #57579e;
+        }
+
+        QPushButton
+        {
+            height: 20px;
+            font-size: 9pt;   
+            background: #babdb6;
+        }
+        QPushButton:hover
+        {   
+            color: black;
+            background: #729fcf;           
+        }
+
+        QComboBox
+        {
+            height: 22px;
+            font-size: 9pt;
+            background: #babdb6;
+        }
+        QComboBox:hover
+        {   
+            color: grey;
+            background: #729fcf;           
+        }
+        QComboBox:item:hover
+        {   
+            color: grey;
+            background: #204a87;           
+        }       
+        QMainWindow
+        {
+         background: qlineargradient(y1: 0, y2: 1,
+                                     stop: 0 #E1E1E1, stop: 0.4 #DDDDDD,
+                                     stop: 0.5 #D8D8D8, stop: 1.0 #D3D3D3);
+        }
+        QLineEdit
+        {   
+            height: 22px;
+             background: qlineargradient(y1: 0, y2: 1,
+                                         stop: 0 #d3d7cf, stop: 1.0 #babdb6);
+}
+    """
+###################################     
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    app.setApplicationName('MediathekQuery')
+    main = MyWindow("")
+    main.show()
+    sys.exit(app.exec_())
